59e5a31ef7622250ba15e0711ce314b2592562aa
Matt Pityo <mpityo@gmail.com>
2022-03-02 18:07:16 -0500

    validation for parameters in post

--- server.js
+++ server.js
@@ -11,6 +11,23 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+function validateAnimal(animal) {
+  // check to make sure the values are filled and they're strings
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
   let filteredResults = animalsArray;
@@ -84,9 +101,15 @@ app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
   
-  // add animal to json file and animals array in this function
-  const animal = createNewAnimal(req.body, animals);
-  res.json(animal);
+  // if any data in req.body is incorrect, send 400 error back
+  // 400 error tells user that the server is OK and it was a user error
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal); 
+  }
 });
 
 app.listen(PORT, () => {
